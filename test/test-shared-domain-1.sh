5756bedc6ea3df79a3892e22c57b51b5facd20e2
Jia Zhu <zhujia.zj@bytedance.com>
2022-08-31 17:46:41 +0800

    test: add shared domain test 01
    Signed-off-by: Jia Zhu <zhujia.zj@bytedance.com>

---
+++ test/test-shared-domain-1.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# Test shared domain feature.
+
+fscachedir="/root"
+_bootstrap="test.img"
+_datablob="blob1.img"
+
+make > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "make failed"
+	exit
+fi
+
+_volume="erofs,$_bootstrap"
+volume="I$_volume"
+
+bootstrap_fan=$(../getfan $_volume $_bootstrap)
+bootstrap_path="$fscachedir/cache/$volume/@$bootstrap_fan/D$_bootstrap"
+
+datablob_fan=$(../getfan $_volume $_datablob)
+datablob_path="$fscachedir/cache/$volume/@$datablob_fan/D$_datablob"
+
+rm -f $bootstrap_path
+rm -f $datablob_path
+
+cd ..
+./cachefilesd2 $fscachedir > /dev/null  &
+cd test
+
+sleep 2
+
+# test multidev data layout
+rm -f $bootstrap_path
+rm -f $datablob_path
+cp img/multidev/test.img ../
+cp img/multidev/blob1.img ../
+
+mount -t erofs none -o fsid=test.img,device=blob1.img,domain_id=domain0 /mnt/
+if [ $? -ne 0 ]; then
+	echo "[shared domain] mount failed"
+	pkill cachefilesd2
+	exit
+fi
+
+content="$(cat /mnt/stamp-h1)"
+if [ $? -ne 0 ]; then
+	echo "[shared domain] read failed"
+	umount /mnt
+	pkill cachefilesd2
+	exit
+fi
+
+if [ "$content" != "timestamp for config.h" ]; then
+	echo "[shared domain] data corruption ($content)"
+	umount /mnt
+	pkill cachefilesd2
+	exit
+fi
+
+mkdir -p mnt-domain
+cp ../test.img ../test2.img
+
+mount -t erofs none -o fsid=test2.img,device=blob1.img,domain_id=domain0 mnt-domain
+
+content="$(cat mnt-domain/stamp-h1)"
+if [ $? -ne 0 ]; then
+	echo "[shared domain] read failed"
+	umount /mnt
+	umount mnt-domain
+	pkill cachefilesd2
+	exit
+fi
+
+if [ "$content" != "timestamp for config.h" ]; then
+	echo "[shared domain] data corruption ($content)"
+	umount /mnt
+	umount mnt-domain
+	pkill cachefilesd2
+	exit
+fi
+
+umount /mnt
+umount mnt-domain
+echo "[shared domain] pass"
+
+#cleanup
+pkill cachefilesd2
+rmdir mnt-domain
